16049e1033b0b3464a3fcf48aea7fe3ffb54afb6
Pdevipriya23 <59272011+Pdevipriya23@users.noreply.github.com>
2019-12-30 04:22:19 -0800

    Add files via upload

---
+++ TestUtility.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class TestUtility 
+{
+	Utility u =null;
+	@Before
+     public void setUp()
+     {
+	     u=new Utility();
+}
+@After
+	public void tearDown()
+	{
+		u=null;
+	}
+	@Test
+	public void testSqr() 
+	{
+		assertEquals(100,u.sqr(10));
+	}
+	public void testSum()
+	{
+		assertEquals(10,u.sum(20,-10));
+	}
+}
