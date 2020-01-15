3b29dc8ac6d1d6110845765d868e57fc81186d99
Pdevipriya23 <59272011+Pdevipriya23@users.noreply.github.com>
2020-01-15 16:17:23 +0530

    Add files via upload

---
+++ Action.c
@@ -0,0 +1,25 @@
+Action()
+{
+
+	web_url("nlr_start_transaction("");
+exlr_start_transaction("");
+wlr_start_transaction("");
+avelearn.com", 
+		"URL=http://nexwavelearn.com/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=http://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxM.woff", ENDITEM, 
+		"Url=http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/fonts/fontawesome-webfont.eot?", ENDITEM, 
+		"Url=/images/footer-bg.jpg", ENDITEM, 
+		"Url=/images/divider.jpg", ENDITEM, 
+		"Url=/images/md/slide-1.jpg", ENDITEM, 
+		"Url=/images/social-sprites.png", ENDITEM, 
+		LAlr_end_transaction("", LR_AUTO);
+ST);
+
+	return 0;
+}
